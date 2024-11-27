cca858801a8d04c17eb4643fa90ff92750a19a50
Lucas Fogaça <lucas.fogaca@vsoft.com.br>
2024-11-27 19:33:56 -0300

    Atualiza informações de versão e configurações do projeto para compatibilidade com .NET 9.0

--- obj/Debug/net8.0/MinhaPrimeiraAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinhaPrimeiraAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPrimeiraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a0a7f697c24978aca3d1083970604633097ff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPrimeiraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPrimeiraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
